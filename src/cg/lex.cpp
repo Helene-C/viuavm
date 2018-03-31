34e61485d1dc51fdbf47e2240b1e60232464c080
Marek Marecki <marekjm@ozro.pw>
2018-03-31 13:39:20 +0200

    Fix normalisation of "string" instruction

--- src/cg/lex.cpp
+++ src/cg/lex.cpp
@@ -908,6 +908,8 @@ auto standardise(vector<Token> input_tokens) -> vector<Token> {
                 tokens.emplace_back(input_tokens.at(i).line(),
                                     input_tokens.at(i).character(),
                                     "local");
+            } else {
+                tokens.push_back(input_tokens.at(++i));  // target register set specifier
             }
             if (input_tokens.at(i + 1) == "\n") {
                 tokens.emplace_back(input_tokens.at(i).line(),
